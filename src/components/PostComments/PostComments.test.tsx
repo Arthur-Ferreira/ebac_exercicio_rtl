c25207de30017ed6e4151bd618f96259b46035a8
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-01-24 17:07:36 -0300

    Exercise upload with one test that add two new comments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -8,33 +8,24 @@ describe("Teste para o componente PostComment", () => {
     expect(screen.getByText("Comentar")).toBeInTheDocument();
   });
 
-  it("Deve adicionar o primeiro comentário", () => {
-    render(<Post />);
+  it("Deve adicionar os novos comentários", () => {
+    render(<PostComment />);
 
     // Encontrar o textarea e simular a digitação de um comentário
     const textarea = screen.getByRole("textbox");
     fireEvent.change(textarea, { target: { value: "Primeiro Comentário" } });
-
+    
     // Encontrar o botão de envio e simular o clique
     const button = screen.getByTestId("add-comment");
     fireEvent.click(button);
-
-    // Verificar se o novo comentário foi adicionado à lista
-    expect(screen.getByText("Primeiro Comentário")).toBeInTheDocument();
-  });
-
-  it("Deve adicionar o segundo comentário", () => {
-    render(<Post />);
-
-    // Encontrar o textarea e simular a digitação de um comentário
-    const textarea = screen.getByRole("textbox");
+    
+    // Usa o textarea já mapeado para simular a digitação de um novo comentário
     fireEvent.change(textarea, { target: { value: "Segundo Comentário" } });
 
-    // Encontrar o botão de envio e simular o clique
-    const button = screen.getByTestId("add-comment");
+    // Simular o clique
     fireEvent.click(button);
 
-    // Verificar se o novo comentário foi adicionado à lista
-    expect(screen.getByText("Segundo Comentário")).toBeInTheDocument();
+    // Verificar se os novos comentários foram adicionados à lista
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
   });
 });
