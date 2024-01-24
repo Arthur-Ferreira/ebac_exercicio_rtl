f208ddd2946f771003680df585f28311839b78ad
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-01-24 16:36:01 -0300

    Exercise upload

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,40 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve adicionar o primeiro comentário", () => {
+    render(<Post />);
+
+    // Encontrar o textarea e simular a digitação de um comentário
+    const textarea = screen.getByRole("textbox");
+    fireEvent.change(textarea, { target: { value: "Primeiro Comentário" } });
+
+    // Encontrar o botão de envio e simular o clique
+    const button = screen.getByTestId("add-comment");
+    fireEvent.click(button);
+
+    // Verificar se o novo comentário foi adicionado à lista
+    expect(screen.getByText("Primeiro Comentário")).toBeInTheDocument();
+  });
+
+  it("Deve adicionar o segundo comentário", () => {
+    render(<Post />);
+
+    // Encontrar o textarea e simular a digitação de um comentário
+    const textarea = screen.getByRole("textbox");
+    fireEvent.change(textarea, { target: { value: "Segundo Comentário" } });
+
+    // Encontrar o botão de envio e simular o clique
+    const button = screen.getByTestId("add-comment");
+    fireEvent.click(button);
+
+    // Verificar se o novo comentário foi adicionado à lista
+    expect(screen.getByText("Segundo Comentário")).toBeInTheDocument();
+  });
 });
